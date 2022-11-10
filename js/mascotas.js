0ecbb2043ff92566875afe147bcd8f49fe25eaee
DvncanG <dvncan30@gmail.com>
2022-11-10 16:55:13 +0100

    modificaciones

--- js/mascotas.js
+++ js/mascotas.js
@@ -9,13 +9,13 @@ export class Mascotas {
         this._precio = precio
     }
 
-    get nombre(){ return this.nombre}
-    get tipo(){ return this.tipo}
+    get nombre(){ return this._nombre}
+    get tipo(){ return this._tipo}
     get fechaNacimiento(){return this._fechaNacimiento}
     get precio(){return this._precio}
 
-    set nombre(nombre){this.nombre}
-    set tipo(tipo){this.tipo}
+    set nombre(nombre){this._nombre}
+    set tipo(tipo){this._tipo}
     set fechaNacimiento(fechaNacimiento){this._fechaNacimiento}
     set precio(precio){this._precio}
 
@@ -28,19 +28,19 @@ export class Perro extends Mascotas{
     get chip(){return this._chip}
     get pureza(){return this._pureza}
 
-    set chip(chip){this.chip}
-    set pureza(pureza){this.pureza}
+    set chip(chip){this._chip}
+    set pureza(pureza){this._pureza}
 }
-export class PerroCaza extends Mascotas{
-    constructor(sexo,raza,chip,pureza){
-        super(chip,pureza)
+export class PerroCaza extends Perro{
+    constructor(sexo,raza,chip,pureza,nombre, tipo, fechaNacimiento, precio){
+        super(chip,pureza,nombre, tipo, fechaNacimiento, precio)
 
     }
     get sexo(){return this._sexo}
     get raza(){return this._raza}
 
-    set sexo(sexo){this.sexo}
-    set raza(raza){this.raza}
+    set sexo(sexo){this._sexo}
+    set raza(raza){this._raza}
 }
 
 export class Gato extends Mascotas{
@@ -51,8 +51,8 @@ export class Gato extends Mascotas{
     get sexo(){return this._sexo}
     get estado(){return this._estado}
 
-    set sexo(sexo){this.sexo}
-    set estado(estado){this.estado}
+    set sexo(sexo){this._sexo}
+    set estado(estado){this._estado}
 }
 
 export class Aves extends Mascotas{
@@ -63,6 +63,6 @@ export class Aves extends Mascotas{
     get continente(){return this._continente}
     get alimentacion(){return this._alimentacion}
 
-    set continente(continente){this.continente}
-    set alimentacion(alimentacion){this.alimentacion}
+    set continente(continente){this._continente}
+    set alimentacion(alimentacion){this._alimentacion}
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,43 @@
-import { Aves, Mascotas } from "./mascotas";
-import { Gato } from "./mascotas";
-import { Perro } from "./mascotas";
-import { PerroCaza } from "./mascotas";
+import { Aves } from "./mascotas.js";
+import { Gato } from "./mascotas.js";
+import { Perro } from "./mascotas.js";
+import { PerroCaza } from "./mascotas.js";
 
 const mascotas = [];
+const Pedrito = new Perro(1,1,"Pedrito","Perro","3/12/1998","5€")
+const Nevado = new Perro(2,3,"Nevado","Perro","17/08/1996","100.99")
+
+const Bilal = new PerroCaza("Masculino","Soughli",2,3,"Bilal","Perro Caza","01/01/2002",200)
+const PacoJones = new PerroCaza("Masculino","Pastor Alemán",5,5,"PacoJones","Perro Caza","02/02/1999",500)
+
+const Chuki = new Aves("Europa","seca","Chuki","Ave","01/01/2000",100)
+const Morty = new Aves("América","húmeda","Morty","Ave","01/01/2010",200)
+
+const Rico = new Gato("Masculino","Bueno","Rico","Gato","21/12/2009",200)
+const Mang = new Gato("Femenino","Bueno","Mang","Gato","21/12/2020",900)
+mascotas.push(Pedrito,Nevado,Bilal,PacoJones,Chuki,Morty,Rico,Mang)
+console.log(mascotas);
+
+
+const mostarMascotas = (ArrayAnimales) =>{
+
+    ArrayAnimales.map(element =>{
+        console.log("Datos de la Mascota: "+element.nombre)
+        console.log("Nacimiento: "+element.fechaNacimiento)
+        console.log("Tipo: "+element.tipo)
+        console.log("Precio: "+element.precio)
+        console.log("////////////////////////////////////////////////")
+
+    })
+
+}
+const cambiarPrecio = (mascota,nuevoPrecio) =>{
+    console.log(mascota)
+    mascotas[mascota].precio = nuevoPrecio
+    console.log(mascotas[mascota].precio)
+    console.log(nuevoPrecio)
+}
+mostarMascotas(mascotas)
+mostarMascotas(mascotas.filter(mascotas => mascotas.nombre == "Chuki"))
+cambiarPrecio(mascotas.findIndex(mascotas => mascotas.nombre == "Chuki"),600)
+mostarMascotas(mascotas.filter(mascotas => mascotas.nombre == "Chuki"))
